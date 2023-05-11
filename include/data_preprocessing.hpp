24088425df2e8bcb321591008a2123f64a2f85aa
cuterwrite <158505862@qq.com>
2023-05-11 11:49:27 +0800

    项目初始化

--- include/data_preprocessing.hpp
+++ include/data_preprocessing.hpp
@@ -13,7 +13,33 @@
 class DataPreprocessor {
 public:
     // 构造函数
-    DataPreprocessor();
+    DataPreprocessor() = default;
+
+    /**
+     * 从文件中读取输入数据
+     * 参数：
+     *  - filename: 数据文件路径
+     * 返回：读取的数据
+    */
+    Eigen::MatrixXd read_input_data(const std::string& filename);
+
+    /**
+     * 选择一个较小的样本子集
+     * 参数：
+     *  - data: 输入数据
+     * - subset_size: 子集大小
+     * 返回：选定的子集
+    */
+    Eigen::MatrixXd select_subset(const Eigen::MatrixXd& data, unsigned int subset_size);
+
+    /**
+     * 计算特征矩阵的协方差矩阵
+     * 参数：
+     *  - data： 输入数据
+     * 返回：协方差矩阵
+    */
+    Eigen::MatrixXd compute_covariance_matrix(const Eigen::MatrixXd& data);
+
     /**
      * 使用PCA方法降维
      * 参数： 
@@ -44,6 +70,21 @@ Eigen::MatrixXd DataPreprocessor::reduce_dimensionality(const Eigen::MatrixXd& d
 std::vector<unsigned int> DataPreprocessor::estimate_num_clusters(const Eigen::MatrixXd& data, unsigned int min_clusters, unsigned int max_clusters) {
     // @TODO
     return std::vector<unsigned int>();
+}
+
+Eigen::MatrixXd DataPreprocessor::read_input_data(const std::string& filename) {
+    // @TODO
+    return Eigen::MatrixXd();
+}
+
+Eigen::MatrixXd DataPreprocessor::select_subset(const Eigen::MatrixXd& data, unsigned int subset_size) {
+    // @TODO
+    return Eigen::MatrixXd();
+}
+
+Eigen::MatrixXd DataPreprocessor::compute_covariance_matrix(const Eigen::MatrixXd& data) {
+    // @TODO
+    return Eigen::MatrixXd();
 }
 
 #endif // __DATA_PREPROCESSING_HPP__
---
+++ include/utils.hpp
@@ -0,0 +1,48 @@
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
+#include<string>
+#include<eigen3/Eigen/Dense>
+
+namespace utils {
+    /**
+     * 从文件中读取数据并存储为Eigen矩阵
+     * 参数：
+     *  - filename: 文件名
+     * 返回：从文件中读取的数据矩阵
+     */
+    Eigen::MatrixXd read_data_from_file(const std::string& filename);
+
+    /**
+     * 将矩阵数据写入文件
+     * 参数：
+     *  - filename: 文件名
+     *  - data: 要写入文件的数据矩阵
+     */
+    void write_data_to_file(const std::string& filename, const Eigen::MatrixXd& data);
+
+    /**
+     * 计算两点之间的欧氏距离
+     * 参数：
+     *  - point1: 点1
+     *  - point2: 点2
+     * 返回：两点之间的欧氏距离
+     */
+    double euclidean_distance(const Eigen::VectorXd& point1, const Eigen::VectorXd& point2);
+} // namespace utils
+
+Eigen::MatrixXd utils::read_data_from_file(const std::string& filenmae) {
+    // @TODO
+    return Eigen::MatrixXd();
+}
+
+void utils::write_data_to_file(const std::string& filename, const Eigen::MatrixXd& data) {
+    // @TODO
+}
+
+double utils::euclidean_distance(const Eigen::VectorXd& point1, const Eigen::VectorXd& point2) {
+    // @TODO
+    return 0.0;
+}
+
+#endif // __UTILS_HPP__
